a526da2f748da6a4b0d25fc226a11d2a820cc67d
Jongmin Lee <lonslonz@sk.com>
2013-08-23 16:43:45 +0900

    db

--- sql/create.sql
+++ sql/create.sql
@@ -1,3 +1,19 @@
+/*
+SQLyog Community v10.1 Beta1
+MySQL - 5.5.21 : Database - mail
+*********************************************************************
+*/
+
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `tb_mail` */
+
 CREATE TABLE `tb_mail` (
   `mail_id` int(11) NOT NULL AUTO_INCREMENT,
   `sender` varchar(128) DEFAULT NULL,
@@ -14,4 +30,18 @@ CREATE TABLE `tb_mail` (
   `save_time` datetime DEFAULT NULL,
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`mail_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=128 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `tb_rcpt_alias` */
+
+CREATE TABLE `tb_rcpt_alias` (
+  `rcpt_id` int(11) NOT NULL AUTO_INCREMENT,
+  `alias` varchar(128) NOT NULL,
+  `rcpt_addr` varchar(128) NOT NULL,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`rcpt_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
